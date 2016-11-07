cf4f5810735cd09d76080946eca329135b378e8f
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-11-07 11:13:29 +0100

    added `which()` global function and its test

---
+++ src/Core/global_functions.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of cakephp-mysql-backup.
+ *
+ * cakephp-mysql-backup is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * cakephp-mysql-backup is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with cakephp-mysql-backup.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author      Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright   Copyright (c) 2016, Mirko Pagliai for Nova Atlantis Ltd
+ * @license     http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link        http://git.novatlantis.it Nova Atlantis Ltd
+ */
+
+if (!function_exists('which')) {
+    /**
+     * Executes the `which` command and shows the full path of (shell) commands
+     * @param string $command Command
+     * @return string
+     */
+    function which($command)
+    {
+        return exec(sprintf('which %s', $command));
+    }
+}
