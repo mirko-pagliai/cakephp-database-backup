d9f2acd451a77114a8b2cab73efbb6fa5030e962
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-11-08 11:28:17 +0100

    fixed for mysql

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -101,7 +101,7 @@ Cache::config([
 
 // Ensure default test connection is defined
 if (!getenv('db_dsn')) {
-    putenv('db_dsn=sqlite:///:memory:');
+    putenv('db_dsn=mysql://travis@localhost/test');
 }
 
 ConnectionManager::config('test', ['url' => getenv('db_dsn')]);
