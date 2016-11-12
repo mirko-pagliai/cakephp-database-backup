e07dd2fb7b0e7cae4b59859296e346a5014267e8
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-11-12 11:41:21 +0100

    fixed

--- tests/TestCase/Core/GlobalFunctionsTest.php
+++ tests/TestCase/Core/GlobalFunctionsTest.php
@@ -20,7 +20,7 @@
  * @license     http://www.gnu.org/licenses/agpl.txt AGPL License
  * @link        http://git.novatlantis.it Nova Atlantis Ltd
  */
-namespace MysqlBackup\Test\TestCase;
+namespace MysqlBackup\Test\TestCase\Core;
 
 use Cake\TestSuite\TestCase;
 
