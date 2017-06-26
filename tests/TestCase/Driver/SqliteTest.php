42d81f314b37846e35239da0a6d679330a9f2bda
mirko-pagliai <mirko.pagliai@gmail.com>
2017-06-26 14:48:08 +0200

    fixed code

--- tests/TestCase/Driver/SqliteTest.php
+++ tests/TestCase/Driver/SqliteTest.php
@@ -42,11 +42,6 @@ class SqliteTest extends DriverTestCase
     protected $Sqlite;
 
     /**
-     * @var bool
-     */
-    public $autoFixtures = false;
-
-    /**
      * Fixtures
      * @var array
      */
@@ -77,8 +72,6 @@ class SqliteTest extends DriverTestCase
     public function tearDown()
     {
         parent::tearDown();
-
-        Configure::write(DATABASE_BACKUP . '.connection', 'test');
 
         unset($this->Sqlite);
     }
