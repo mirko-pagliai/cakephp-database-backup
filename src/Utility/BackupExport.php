7f24b39d00077c7d2957f37b300cc907881be2a6
mirko-pagliai <mirko.pagliai@gmail.com>
2017-06-27 13:13:26 +0200

    fixed links to wiki

--- src/Utility/BackupExport.php
+++ src/Utility/BackupExport.php
@@ -19,7 +19,7 @@
  * @copyright   Copyright (c) 2016, Mirko Pagliai for Nova Atlantis Ltd
  * @license     http://www.gnu.org/licenses/agpl.txt AGPL License
  * @link        http://git.novatlantis.it Nova Atlantis Ltd
- * @see         https://github.com/mirko-pagliai/cakephp-mysql-backup/wiki/How-to-use-the-BackupExport-utility
+ * @see         https://github.com/mirko-pagliai/cakephp-database-backup/wiki/How-to-use-the-BackupExport-utility
  */
 namespace DatabaseBackup\Utility;
 
@@ -105,7 +105,7 @@ class BackupExport
      * @param bool|string $compression Compression type. Supported values are
      *  `bzip2`, `gzip` and `false` (if you don't want to use compression)
      * @return \DatabaseBackup\Utility\BackupExport
-     * @see https://github.com/mirko-pagliai/cakephp-mysql-backup/wiki/How-to-use-the-BackupExport-utility#compression
+     * @see https://github.com/mirko-pagliai/cakephp-database-backup/wiki/How-to-use-the-BackupExport-utility#compression
      * @throws InternalErrorException
      * @uses $compression
      * @uses $driver
@@ -131,7 +131,7 @@ class BackupExport
      * @param string $filename Filename. It can be an absolute path and may
      *  contain patterns
      * @return \DatabaseBackup\Utility\BackupExport
-     * @see https://github.com/mirko-pagliai/cakephp-mysql-backup/wiki/How-to-use-the-BackupExport-utility#filename
+     * @see https://github.com/mirko-pagliai/cakephp-database-backup/wiki/How-to-use-the-BackupExport-utility#filename
      * @throws InternalErrorException
      * @uses compression()
      * @uses $config
@@ -181,7 +181,7 @@ class BackupExport
      * backups that are older
      * @param int $rotate Number of backups you want to keep
      * @return \DatabaseBackup\Utility\BackupExport
-     * @see https://github.com/mirko-pagliai/cakephp-mysql-backup/wiki/How-to-use-the-BackupExport-utility#rotate
+     * @see https://github.com/mirko-pagliai/cakephp-database-backup/wiki/How-to-use-the-BackupExport-utility#rotate
      * @uses $rotate
      */
     public function rotate($rotate)
@@ -208,7 +208,7 @@ class BackupExport
     /**
      * Exports the database
      * @return string Filename path
-     * @see https://github.com/mirko-pagliai/cakephp-mysql-backup/wiki/How-to-use-the-BackupExport-utility#export
+     * @see https://github.com/mirko-pagliai/cakephp-database-backup/wiki/How-to-use-the-BackupExport-utility#export
      * @uses filename()
      * @uses $BackupManager;
      * @uses $driver
--- src/Utility/BackupImport.php
+++ src/Utility/BackupImport.php
@@ -19,7 +19,7 @@
  * @copyright   Copyright (c) 2016, Mirko Pagliai for Nova Atlantis Ltd
  * @license     http://www.gnu.org/licenses/agpl.txt AGPL License
  * @link        http://git.novatlantis.it Nova Atlantis Ltd
- * @see         https://github.com/mirko-pagliai/cakephp-mysql-backup/wiki/How-to-use-the-BackupImport-utility
+ * @see         https://github.com/mirko-pagliai/cakephp-database-backup/wiki/How-to-use-the-BackupImport-utility
  */
 namespace DatabaseBackup\Utility;
 
@@ -60,7 +60,7 @@ class BackupImport
      * Sets the filename
      * @param string $filename Filename. It can be an absolute path
      * @return \DatabaseBackup\Utility\BackupImport
-     * @see https://github.com/mirko-pagliai/cakephp-mysql-backup/wiki/How-to-use-the-BackupImport-utility#filename
+     * @see https://github.com/mirko-pagliai/cakephp-database-backup/wiki/How-to-use-the-BackupImport-utility#filename
      * @throws InternalErrorException
      * @uses $driver
      * @uses $filename
@@ -85,7 +85,7 @@ class BackupImport
     /**
      * Imports the database
      * @return string Filename path
-     * @see https://github.com/mirko-pagliai/cakephp-mysql-backup/wiki/How-to-use-the-BackupImport-utility#import
+     * @see https://github.com/mirko-pagliai/cakephp-database-backup/wiki/How-to-use-the-BackupImport-utility#import
      * @throws InternalErrorException
      * @uses $driver
      * @uses $filename
