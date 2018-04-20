3fe70584e0c5eff134eac4c849ed0fdb796e6f81
mirko-pagliai <mirko.pagliai@gmail.com>
2018-04-20 14:21:12 +0200

    added `Plugin` class for CakePHP 3.6

---
+++ src/Plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of cakephp-database-backup.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/cakephp-database-backup
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ * @since       2.6.0
+ */
+namespace DatabaseBackup;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Plugin class
+ */
+class Plugin extends BasePlugin
+{
+}
