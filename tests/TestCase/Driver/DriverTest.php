e932dafce35c0305c608d911854275fe063aacf5
mirko-pagliai <mirko.pagliai@gmail.com>
2019-12-06 17:17:03 +0100

    fixed

--- tests/TestCase/Driver/DriverTest.php
+++ tests/TestCase/Driver/DriverTest.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 /**
  * This file is part of cakephp-database-backup.
@@ -34,7 +35,7 @@ class DriverTest extends TestCase
      * Called before every test method
      * @return void
      */
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
 
