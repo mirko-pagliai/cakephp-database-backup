1284da0e4ae31f99be966bf8ccd2dc4f9c573414
mirko-pagliai <mirko.pagliai@gmail.com>
2021-09-29 17:25:16 +0200

    added mysql connection

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -84,7 +84,7 @@ Cache::setConfig([
 ]);
 
 if (!getenv('db_dsn')) {
-    putenv('db_dsn=mysql://travis@localhost/test');
+    putenv('db_dsn=mysql://root:root@127.0.0.1/test');
 }
 if (!getenv('db_dsn_postgres')) {
     putenv('db_dsn_postgres=postgres://postgres@localhost/travis_ci_test');
