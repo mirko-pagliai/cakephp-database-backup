5f93fab7dc9cc04cf8bf539cddeffcbb4bc987b4
mirko-pagliai <mirko.pagliai@gmail.com>
2023-06-06 18:17:54 +0200

    fixed

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -38,8 +38,8 @@ if (!defined('DATABASE_BACKUP_EXTENSIONS')) {
 
 //Writes default configuration values
 $defaults = [
-    'DatabaseBackup.connection' => 'default',
     'DatabaseBackup.chmod' => 0664,
+    'DatabaseBackup.connection' => 'default',
     'DatabaseBackup.processTimeout' => 60,
     'DatabaseBackup.target' => ROOT . 'backups',
     'DatabaseBackup.mysql.export' => '{{BINARY}} --defaults-file={{AUTH_FILE}} {{DB_NAME}}',
