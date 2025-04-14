ba4d10e50bb21cf3a73b9da25fa6104923c6cf88
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-04-14 20:34:45 +0200

    all PHPUnit deprecations have finally been removed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,7 @@
   Tests involving "Executor" classes do not use this class, as they have no need for it. The `PostgresExecutorTest` test
   class has also been removed, since the class it covers, by default, does not implement anything that the parent class
   does not;
+* all PHPUnit deprecations have finally been removed;
 * added `symfony/polyfill-php83`. This allowed the `#[Override]` attribute to be added to all affected methods;
 * we also begin to apply the `UsesClass` attribute and remove the old `@uses` tag;
 * updated for `cakedc/cakephp-phpstan` to `^4.0` (and so `phpstan/phpstan` to `2.1.8`).
--- tests/TestCase/Utility/BackupExportAndImportTest.php
+++ tests/TestCase/Utility/BackupExportAndImportTest.php
@@ -17,20 +17,21 @@ namespace DatabaseBackup\Test\TestCase\Utility;
 
 use Cake\Datasource\ConnectionManager;
 use Cake\ORM\Table;
+use Cake\TestSuite\TestCase;
 use DatabaseBackup\Compression;
-use DatabaseBackup\TestSuite\TestCase;
 use DatabaseBackup\Utility\BackupExport;
 use DatabaseBackup\Utility\BackupImport;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestWith;
 
 /**
  * BackupExportAndImportTest class.
  *
  * Performs tests common to the `BackupExport` and `BackupImport` classes.
- *
- * @uses \DatabaseBackup\Utility\BackupExport
- * @uses \DatabaseBackup\Utility\BackupImport
  */
+#[CoversClass(BackupExport::class)]
+#[CoversClass(BackupImport::class)]
 class BackupExportAndImportTest extends TestCase
 {
     /**
@@ -39,17 +40,12 @@ class BackupExportAndImportTest extends TestCase
     protected Table $Articles;
 
     /**
-     * @var \DatabaseBackup\Utility\BackupExport
-     */
-    protected BackupExport $BackupExport;
-
-    /**
      * @var \Cake\ORM\Table
      */
     protected Table $Comments;
 
     /**
-     * @inheritDoc
+     * @var array<string>
      */
     public array $fixtures = [
         'core.Articles',
@@ -77,84 +73,70 @@ class BackupExportAndImportTest extends TestCase
     {
         parent::setUp();
 
-        $this->BackupExport = new BackupExport();
-
-        /** @var \Cake\Database\Connection $connection */
-        $connection = ConnectionManager::get('test');
+        /** @var \Cake\Database\Connection $Connection */
+        $Connection = ConnectionManager::get('test');
 
         foreach (['Articles', 'Comments'] as $name) {
-            $this->{$name} ??= $this->fetchTable($name, compact('connection'));
+            $this->{$name} ??= $this->fetchTable($name, compact('Connection'));
         }
     }
 
     /**
-     * @uses \DatabaseBackup\Utility\AbstractBackupUtility::__get()
+     * Test for `BackupExport::export()` and `BackupImport::import()` methods.
+     *
+     * It tests that the backup is properly exported and then imported.
      */
     #[Test]
-    public function testGetMagicMethod(): void
+    #[TestWith([Compression::None])]
+    #[TestWith([Compression::Gzip])]
+    #[TestWith([Compression::Bzip2])]
+    public function testExportAndImport(Compression $Compression): void
     {
-        $this->BackupExport->rotate(2);
-        // @phpstan-ignore property.protected
-        $this->assertSame(2, $this->BackupExport->rotate);
-
-        $this->expectExceptionMessage('Undefined property: ' . BackupExport::class . '::$noExistingProperty');
-        // @phpstan-ignore property.notFound, expr.resultUnused
-        $this->BackupExport->noExistingProperty;
-    }
-
-    /**
-     * Test for `export()` and `import()` methods. It tests that the backup is properly exported and then imported.
-     *
-     * @test
-     * @uses \DatabaseBackup\Utility\BackupExport::export()
-     * @uses \DatabaseBackup\Utility\BackupImport::import()
-     */
-    public function testExportAndImport(): void
-    {
+        $BackupExport = new BackupExport();
         $BackupImport = new BackupImport();
 
-        foreach (Compression::cases() as $Compression) {
-            $expectedFilename = TMP . 'backups' . DS . 'backup_' . uniqid('example_') . '.' . $Compression->value;
+        $expectedFilename = TMP . 'backups' . DS . 'backup_' . uniqid('example_') . '.' . $Compression->value;
 
-            //Initial records. 3 articles and 6 comments
-            $initial = $this->getAllRecords();
-            $this->assertCount(3, $initial['Articles']);
-            $this->assertCount(6, $initial['Comments']);
+        //Initial records. 3 articles and 6 comments
+        $initial = $this->getAllRecords();
+        $this->assertCount(3, $initial['Articles']);
+        $this->assertCount(6, $initial['Comments']);
 
-            //Exports backup and deletes article with ID 2 and comment with ID 4
-            $result = $this->BackupExport->filename($expectedFilename)->export();
-            $this->assertSame($expectedFilename, $result);
-            $this->Articles->delete($this->Articles->get(2), ['atomic' => false]);
-            $this->Comments->delete($this->Comments->get(4), ['atomic' => false]);
+        //Exports backup and deletes article with ID 2 and comment with ID 4
+        $result = $BackupExport->filename($expectedFilename)->export();
+        $this->assertSame($expectedFilename, $result);
+        $this->Articles->delete($this->Articles->get(2), ['atomic' => false]);
+        $this->Comments->delete($this->Comments->get(4), ['atomic' => false]);
 
-            //Records after delete. 2 articles and 5 comments
-            $afterDelete = $this->getAllRecords();
-            $this->assertCount(count($initial['Articles']) - 1, $afterDelete['Articles']);
-            $this->assertCount(count($initial['Comments']) - 1, $afterDelete['Comments']);
+        //Records after delete. 2 articles and 5 comments
+        $afterDelete = $this->getAllRecords();
+        $this->assertCount(count($initial['Articles']) - 1, $afterDelete['Articles']);
+        $this->assertCount(count($initial['Comments']) - 1, $afterDelete['Comments']);
 
-            //Imports backup. Now initial records are the same of final records
-            $result = $BackupImport->filename($expectedFilename)->import();
-            $this->assertSame($expectedFilename, $result);
-            $final = $this->getAllRecords();
-            $this->assertEquals($initial, $final);
+        //Imports backup. Now initial records are the same of final records
+        $result = $BackupImport->filename($expectedFilename)->import();
+        $this->assertSame($expectedFilename, $result);
+        $final = $this->getAllRecords();
+        $this->assertEquals($initial, $final);
 
-            //Gets the difference (`$diff`) between records after delete and records after import (`$final`)
-            $diff = $final;
-            foreach ($final as $model => $finalValues) {
-                foreach ($finalValues as $finalKey => $finalValue) {
-                    foreach ($afterDelete[$model] as $deletedValue) {
-                        if ($finalValue == $deletedValue) {
-                            unset($diff[$model][$finalKey]);
-                        }
+        //Gets the difference (`$diff`) between records after delete and records after import (`$final`)
+        $diff = $final;
+        foreach ($final as $model => $finalValues) {
+            foreach ($finalValues as $finalKey => $finalValue) {
+                foreach ($afterDelete[$model] as $deletedValue) {
+                    if ($finalValue == $deletedValue) {
+                        unset($diff[$model][$finalKey]);
                     }
                 }
             }
-            $this->assertCount(1, $diff['Articles']);
-            $this->assertCount(1, $diff['Comments']);
-
-            //Difference is article with ID 2 and comment with ID 4
-            $this->assertSame([2], array_column($diff['Articles'], 'id'));
-            $this->assertSame([4], array_column($diff['Comments'], 'id'));
         }
+        $this->assertCount(1, $diff['Articles']);
+        $this->assertCount(1, $diff['Comments']);
+
+        //Difference is article with ID 2 and comment with ID 4
+        $this->assertSame([2], array_column($diff['Articles'], 'id'));
+        $this->assertSame([4], array_column($diff['Comments'], 'id'));
+
+        unlink($expectedFilename);
     }
 }
