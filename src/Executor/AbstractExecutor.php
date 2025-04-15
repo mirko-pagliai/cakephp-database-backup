c4eee2a4d68dd02b4677db1a5765197b3c141a4a
mirko-pagliai <mirko.pagliai@gmail.com>
2025-04-15 15:43:53 +0200

    fixed

--- src/Executor/AbstractExecutor.php
+++ src/Executor/AbstractExecutor.php
@@ -220,25 +220,46 @@ abstract class AbstractExecutor implements EventListenerInterface
     }
 
     /**
-     * Finds e returns a binary
+     * Finds and returns an executable binary by name.
      *
-     * @param \DatabaseBackup\Compression|string $name
-     * @return string|null
+     * For example, with `mariadb` it should return `/usr/bin/mariadb`.
+     *
+     * It first checks and returns any value set by the configuration. If not present, it uses `ExecutableFinder::find)`.
+     * If the binary cannot be found, an exception is thrown.
+     *
+     * You can specify more than one name (for example, if there are possible aliases or fallbacks). In this case, the
+     *  first one found is returned.
+     *
+     * @param \DatabaseBackup\Compression|string ...$name
+     * @return string
+     * @since 2.14.1
+     * @throws \InvalidArgumentException
      */
-    public function findBinary(Compression|string $name): ?string
+    public function findBinary(Compression|string ...$name): string
     {
-        if ($name instanceof Compression) {
-            $name = lcfirst($name->name);
+        $name = array_map(
+            callback: fn (Compression|string $name): string => $name instanceof Compression ? lcfirst($name->name) : $name,
+            array: $name
+        );
+
+        foreach ($name as $sName) {
+            $binary = Configure::read('DatabaseBackup.binaries.' . $sName);
+            if ($binary) {
+                return $binary;
+            }
+
+            $ExecutableFinder = new ExecutableFinder();
+            $binary = $ExecutableFinder->find(name: $sName);
+            if ($binary) {
+                return $binary;
+            }
         }
 
-        $binary = Configure::read('DatabaseBackup.binaries.' . $name);
-        if ($binary) {
-            return $binary;
-        }
-
-        $ExecutableFinder = new ExecutableFinder();
-
-        return $ExecutableFinder->find(name: $name);
+        throw new InvalidArgumentException(__d(
+            'database_backup',
+            'Binary for `{0}` could not be found. You have to set its path manually',
+            $name
+        ));
     }
 
     /**
