55655a08738551ac8100542dac1baf497b87306d
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-05-17 18:05:59 +0200

    the `Plugin` class no longer implements the `console()` method (which… (#175)
    * the `Plugin` class no longer implements the `console()` method (which described the available commands), leaving the
        application to take care of auto-discovering them
    
    * fixed test

--- tests/TestCase/PluginTest.php
+++ tests/TestCase/PluginTest.php
@@ -3,11 +3,15 @@ declare(strict_types=1);
 
 namespace DatabaseBackup\Test\TestCase;
 
+use App\Application;
 use Cake\Console\CommandCollection;
 use Cake\TestSuite\TestCase;
 use DatabaseBackup\Plugin;
+use DatabaseBackup\Plugin as DatabaseBackup;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 /**
  * PluginTest.
@@ -15,20 +19,33 @@ use PHPUnit\Framework\Attributes\Test;
 #[CoversClass(Plugin::class)]
 class PluginTest extends TestCase
 {
+    /**
+     * Tests that the application console includes all expected commands.
+     *
+     * This method checks if the commands declared in the `Command` directory
+     * are properly registered and accessible in the application's `CommandCollection`.
+     *
+     * @return void
+     */
     #[Test]
     public function testConsole(): void
     {
-        $expectedCommands = [
-            'database_backup.export' => 'DatabaseBackup\Command\ExportCommand',
-            'database_backup.import' => 'DatabaseBackup\Command\ImportCommand',
-            'database_backup.index' => 'DatabaseBackup\Command\IndexCommand',
-        ];
+        //Sets the expected commands from `Command` files
+        $Finder = new Finder();
+        $Finder = $Finder->files()->in(ROOT . 'src/Command')->name('/.+Command\.php/');
+        $expected = array_map(
+            callback: fn(SplFileInfo $File): string => 'database_backup.' . lcfirst($File->getBasename('Command.php')),
+            array: iterator_to_array($Finder),
+        );
 
-        $Plugin = new Plugin();
-        $CommandCollection = $Plugin->console(new CommandCollection());
+        $CommandCollection = new CommandCollection();
+        $App = new Application(CONFIG);
+        $App->addPlugin(DatabaseBackup::class);
+        $App->pluginConsole($CommandCollection);
+        $availableCommands = $CommandCollection->keys();
 
-        foreach ($expectedCommands as $expectedName => $expectedCommand) {
-            $this->assertSame($expectedCommand, $CommandCollection->get($expectedName));
+        foreach ($expected as $expectedCommand) {
+            $this->assertContains($expectedCommand, $availableCommands);
         }
     }
 }
