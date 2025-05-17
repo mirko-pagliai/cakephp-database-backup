388ac030bae5974944bc3576231a082266e25fe5
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-05-17 15:45:51 +0200

    added FakeConnection

---
+++ tests/test_app/TestApp/Database/FakeConnection.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of cakephp-database-backup.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/cakephp-database-backup
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace App\Database;
+
+use Cake\Database\Connection;
+use Cake\Database\Driver\Sqlite;
+
+/**
+ * A fake connection for tests.
+ */
+class FakeConnection extends Connection
+{
+    public function __construct(array $config = [])
+    {
+        $config += [
+            'driver' => Sqlite::class,
+            'database' => 'my_database',
+            'host' => 'my_hostname',
+        ];
+
+        parent::__construct($config);
+    }
+}
