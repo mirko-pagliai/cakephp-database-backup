5c1ca7d353132d4eb5007bc64383cc3ba3d37b06
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-05-21 23:53:58 +0200

    Add filename property with validation to BackupImport
    Introduced a `filename` property with setter validation in the `BackupImport` utility to ensure the file is readable and valid. Added corresponding tests to verify functionality and error handling for unreadable files.

--- src/Utility/BackupImport.php
+++ src/Utility/BackupImport.php
@@ -15,9 +15,37 @@ declare(strict_types=1);
 
 namespace DatabaseBackup\Utility;
 
+use DatabaseBackup\Compression;
+use DatabaseBackup\OperationType;
+use Symfony\Component\Filesystem\Exception\IOException;
+use function Cake\I18n\__d;
+
 /**
  * Utility to import databases.
  */
 class BackupImport extends Utility
 {
+    public string $filename {
+        set(string $filename) {
+            $filename = $this->makeAbsolutePath(path: $filename);
+            if (!is_readable($filename)) {
+                throw new IOException(
+                    __d('database_backup', 'File or directory `{0}` is not readable', $filename)
+                );
+            }
+
+            //This is only useful to possibly throw a `ValueError`
+            Compression::fromFilename($filename);
+
+            $this->filename = $filename;
+        }
+    }
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(OperationType: OperationType::Import);
+    }
 }
---
+++ tests/TestCase/Utility/BackupImportTest.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of cakephp-database-backup.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/cakephp-database-backup
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace DatabaseBackup\Test\TestCase\Utility;
+
+use DatabaseBackup\TestSuite\TestCase;
+use DatabaseBackup\Utility\BackupImport;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestWith;
+use Symfony\Component\Filesystem\Exception\IOException;
+
+/**
+ * BackupImportTest.
+ */
+#[CoversClass(BackupImport::class)]
+class BackupImportTest extends TestCase
+{
+    protected BackupImport $BackupImport;
+
+    /**
+     * @inheritDoc
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->BackupImport = new BackupImport();
+    }
+
+    #[Test]
+    public function testFilename(): void
+    {
+        $filename = TMP . 'backup.sql';
+        file_put_contents($filename, '');
+
+        $this->BackupImport->filename = $filename;
+        unlink($filename);
+
+        $result = $this->BackupImport->filename;
+        $this->assertSame($filename, $result);
+    }
+
+    #[Test]
+    public function testFilenamePropertyNoReadableFile(): void
+    {
+        $filename = TMP . 'noExistingDir/backup.sql';
+
+        $this->expectException(IOException::class);
+        $this->expectExceptionMessage('File or directory `' . $filename . '` is not readable');
+        $this->BackupImport->filename = $filename;
+    }
+}
