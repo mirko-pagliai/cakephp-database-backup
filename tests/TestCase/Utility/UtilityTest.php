7506cfff4a9320fa10224904afd8de6f02c321ca
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-09-23 10:22:28 +0200

    Add `@link` annotations and improve test case structure for `UtilityTest`
    Includes adjustments to exception handling, variable naming, and `DataProvider` output for enhanced clarity and maintainability.

--- tests/TestCase/Utility/UtilityTest.php
+++ tests/TestCase/Utility/UtilityTest.php
@@ -17,6 +17,7 @@ namespace DatabaseBackup\Test\TestCase\Utility;
 
 use App\Database\FakeConnection;
 use App\Executor\FakeExecutor;
+use BadMethodCallException;
 use Cake\Core\Configure;
 use Cake\Datasource\ConnectionInterface;
 use Cake\Datasource\ConnectionManager;
@@ -48,6 +49,9 @@ class UtilityTest extends TestCase
         $this->Utility = new class (OperationType: OperationType::Export) extends Utility {};
     }
 
+    /**
+     * @link \DatabaseBackup\Utility\Utility::$connection
+     */
     #[Test]
     #[TestWith([new FakeConnection()])]
     #[TestWith(['test'])]
@@ -68,6 +72,9 @@ class UtilityTest extends TestCase
         $this->assertSame('test', $result->config()['name']);
     }
 
+    /**
+     * @link \DatabaseBackup\Utility\Utility::$Executor
+     */
     #[Test]
     public function testExecutorProperty(): void
     {
@@ -77,6 +84,9 @@ class UtilityTest extends TestCase
         $this->assertInstanceOf(Executor::class, $result);
     }
 
+    /**
+     * @link \DatabaseBackup\Utility\Utility::$Executor
+     */
     #[Test]
     public function testExecutorPropertySetExecutor(): void
     {
@@ -86,6 +96,9 @@ class UtilityTest extends TestCase
         $this->assertSame($Executor, $this->Utility->Executor);
     }
 
+    /**
+     * @link \DatabaseBackup\Utility\Utility::$Executor
+     */
     #[Test]
     public function testExecutorPropertyNoExistingExecutor(): void
     {
@@ -97,20 +110,26 @@ class UtilityTest extends TestCase
         $this->Utility->Executor;
     }
 
+    /**
+     * @link \DatabaseBackup\Utility\Utility::$timeout
+     */
     #[Test]
     #[TestWith([0])]
     #[TestWith([10])]
-    public function testTimeoutProperty(int $timeOut): void
+    public function testTimeoutProperty(int $timeout): void
     {
         //The default value of the property is obtained from the configuration
         Configure::write('DatabaseBackup.processTimeout', 45);
         $this->assertSame(45, $this->Utility->timeout);
 
         //The value set via the setter will take precedence over the general configuration
-        $this->Utility->timeout = $timeOut;
-        $this->assertSame($timeOut, $this->Utility->timeout);
+        $this->Utility->timeout = $timeout;
+        $this->assertSame($timeout, $this->Utility->timeout);
     }
 
+    /**
+     * @link \DatabaseBackup\Utility\Utility::$timeout
+     */
     #[Test]
     public function testTimeoutPropertyWithInvalidValue(): void
     {
@@ -119,10 +138,13 @@ class UtilityTest extends TestCase
         $this->Utility->timeout = -1;
     }
 
+    /**
+     * @link \DatabaseBackup\Utility\Utility::__call()
+     */
     #[Test]
     public function testCallMagicMethodWithNoExistingMethod(): void
     {
-        $this->expectException(\BadMethodCallException::class);
+        $this->expectException(BadMethodCallException::class);
         $this->expectExceptionMessageMatches('/^Method `DatabaseBackup\\\\Utility\\\\.+noExistingMethod\(\)` does not exist$/');
         /** @phpstan-ignore-next-line */
         $this->Utility->noExistingMethod();
@@ -136,12 +158,15 @@ class UtilityTest extends TestCase
         $basePath = Configure::readOrFail('DatabaseBackup.target');
 
         return [
-            [$basePath . 'relative_file_to_root.txt', 'relative_file_to_root.txt'],
-            [$basePath . 'absolute_file_to_root.txt', $basePath . 'absolute_file_to_root.txt'],
+            [$basePath . 'relative_file_to_target.txt', 'relative_file_to_target.txt'],
+            [$basePath . 'absolute_file_to_target.txt', $basePath . 'absolute_file_to_target.txt'],
             [TMP . 'absolute_tmp_file', TMP . 'absolute_tmp_file'],
         ];
     }
 
+    /**
+     * @link \DatabaseBackup\Utility\Utility::makeAbsolutePath()
+     */
     #[Test]
     #[DataProvider('makeAbsolutePathDataProvider')]
     public function testMakeAbsolutePath(string $expectedAbsolutePath, string $path): void
