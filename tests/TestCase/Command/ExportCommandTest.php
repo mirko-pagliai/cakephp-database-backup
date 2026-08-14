df2ba0f8e306dea0e8af1e396011fbc490b7953e
Mirko Pagliai <mirko.pagliai@gmail.com>
2026-08-14 18:00:03 +0200

    Normalize string concatenation in `ExportCommandTest` for consistency

--- tests/TestCase/Command/ExportCommandTest.php
+++ tests/TestCase/Command/ExportCommandTest.php
@@ -78,7 +78,7 @@ class ExportCommandTest extends TestCase
     {
         $this->exec($this->command . ' --filename ' . DS . 'noExistingDir' . DS . 'backup.sql');
         $this->assertExitError();
-        $this->assertErrorContains('File or directory `'. DS . 'noExistingDir` is not writable');
+        $this->assertErrorContains('File or directory `' . DS . 'noExistingDir` is not writable');
     }
 
     #[Test]
